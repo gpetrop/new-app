8e9aa150483554966d4316c9fd1d773fe2fc6784
Petropoulos George <petropoulosg@intralot.com>
2019-09-28 17:55:11 +0300

    Updates

---
+++ precache-manifest.d47a1df05d16ad574ecf47ee75124961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3d03d71a41ada45479317063b6bc05",
+    "url": "/gpetrop/new-app/index.html"
+  },
+  {
+    "revision": "c8de18b646cf17b31258",
+    "url": "/gpetrop/new-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a8b310b8cb6d430a4486",
+    "url": "/gpetrop/new-app/static/js/2.1caea5c3.chunk.js"
+  },
+  {
+    "revision": "c8de18b646cf17b31258",
+    "url": "/gpetrop/new-app/static/js/main.15dd8fc9.chunk.js"
+  },
+  {
+    "revision": "484584803cbbdca7f53e",
+    "url": "/gpetrop/new-app/static/js/runtime-main.7a9b7427.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/gpetrop/new-app/static/media/logo.25bf045c.svg"
+  }
+]);
